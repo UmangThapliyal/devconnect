143ba69ec6ef1f08d90bc3fc6d0197fc7ace4379
UmangThap <itsmylife>
2019-05-28 10:25:29 +0530

    profile api

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
 const app = express();
+const connectDB = require("./config/db")
+
+//connect database
+connectDB();
+
+//Init Middleware
+app.use(express.json({
+    extended: false
+}))
 
 app.get('/', (re1, resp) => {
     resp.send("server is running");
 });
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 5000;
 
